67e56cd7b8570080235f43bdc7cec00de9a0d881
selfdefence <selfdefence1111@gmail.com>
2018-06-09 10:50:46 +0300

    html minifier eklendi minify edilen partiallar index içerisinde adreslendi

--- app/Gruntfile.js
+++ app/Gruntfile.js
@@ -37,6 +37,20 @@ module.exports = function(grunt) {
                 }
             }
         },
+        htmlmin:{
+            dist:{
+                options:{
+                    removeComments: true,
+                    collapseWhitespace: true
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'src',
+                    src: ['partials/*.html', '*.html'],
+                    dest: 'dist'
+                }]
+            }
+        },
         cssmin:{ //tüm css leri burada minify et
             target:{
                 files: {
@@ -70,13 +84,14 @@ module.exports = function(grunt) {
         }
     });
 
+    grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-http-server');
 
-    grunt.registerTask('default', ["jshint", "concat", "uglify", "cssmin", "clean", "http-server", "watch"]);
+    grunt.registerTask('default', ["jshint", "concat", "uglify", "htmlmin", "cssmin", "clean", "http-server", "watch"]);
 };
