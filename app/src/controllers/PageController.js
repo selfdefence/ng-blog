449e61729bc4e9c7e2d5bee7b0f905e78aa46ddf
selfdefence <selfdefence1111@gmail.com>
2018-06-23 00:12:00 +0300

    post sayfasına detay bilgileri eklendi. (yazar, tarih, okunma sayısı)

--- app/src/controllers/PageController.js
+++ app/src/controllers/PageController.js
@@ -1,6 +1,13 @@
 app.controller('PageController', ['$scope', '$stateParams', 'Post', function ($scope, $stateParams, Post) {
     console.log('in PageController: '+ JSON.stringify($stateParams));
+    $scope.post = {
+        title: 'Title',
+        writer: 'Sait Nuri Uyanık',
+        post_date: '11.02.2018',
+        read_number: '1392'
+    };
+
     // Post.getPost().then(function (res) {
-    //
+    //     console.log(res.data);
     // });
 }]);
